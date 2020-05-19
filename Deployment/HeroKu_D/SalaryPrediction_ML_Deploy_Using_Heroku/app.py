4978992b779c2caed00d816bebdd4f938f6ae6ad
Rakesh06184 <2702rakesh@gmail.com>
2020-05-19 15:28:45 +0530

    Added hiden files

--- Deployment/HeroKu_D/SalaryPrediction_ML_Deploy_Using_Heroku/app.py
+++ Deployment/HeroKu_D/SalaryPrediction_ML_Deploy_Using_Heroku/app.py
@@ -6,11 +6,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
