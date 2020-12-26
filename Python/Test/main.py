30f586d5c4de935ee598c585a99a48a952c0b3c5
RakeshKumar045 <raka06184@gmail.com>
2020-12-26 22:56:44 +0530

    Clean the code

--- Python/Test/main.py
+++ Python/Test/main.py
@@ -39,3 +39,45 @@ def sum_number_1_method(n):
 
 
 print("2nd result : ", sum_number_1_method(num))
+
+
+def summ(t):
+    r = 0;
+    for d in str(t):
+        r = r + int(d)
+    # return r
+    if len(str(r)) == 1:
+        return (r)
+    else:
+        return (summ(r))
+
+
+n = summ(9999)
+print(n)
+
+
+def ft(transaction, threshold):
+    l = []
+    for i in transaction:
+        for j in set(i.split()[:-1]):
+            l.append(j)
+    val = {i: l.count(i) for i in l}
+    res = dict(sorted(val.items(), key=lambda item: item[1], reverse=True))
+    count = 0
+    for k, v in res.items():
+        if (count < threshold):
+            print(k, v)
+            count += 1
+
+
+threshold_value = int(input("Enter the threshold value : "))
+# threshold_value = 2
+
+# tran_value = list(input("Enter the trnsaction details : "))
+
+tran_value = ["99 88 500", "99 99 99", "88 99 400", "12 12 800", "12 15 600", "99 99 99", "88 99 400", "12 12 800",
+              "12 15 600"]
+t = ['99 88 500', '99 99 99', '88 99 400', '12 12 800', '12 15 600']
+
+ft(tran_value, threshold_value)
+# ft(t, threshold_value)
