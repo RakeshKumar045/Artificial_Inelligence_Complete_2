ba34917458abfce70e3047ac90941e1e18d049de
RakeshKumar045 <raka06184@gmail.com>
2021-01-24 13:26:52 +0530

    Clean the code

--- api/FlaskAPI/FlaskAPI.py
+++ api/FlaskAPI/FlaskAPI.py
@@ -1,10 +1,41 @@
+import tkinter as tk
+from tkinter import *
+
 from flask import Flask, render_template, url_for, redirect, jsonify, request
 
 app = Flask(__name__)
 
 
+def test_fun():
+    root = tk.Tk()
+    root.title('Form')
+    root.geometry("250x160")
+
+    # variable
+    var = IntVar()
+    # label
+    l1 = Label(root, text="Radio Buttons working", font=("verdana", 10, "bold"))
+    l1.grid(sticky="w", padx=60)
+
+    # radiobuttons
+    r1 = Radiobutton(root, text="R1", value=1, variable=var)
+    r1.grid(sticky="w", row=1, pady=10)
+
+    # label
+    l2 = Label(root, text="Check Buttons", font=("verdana", 10, "bold"))
+    l2.grid(sticky="w", padx=60)
+
+    # checkbuttons
+    c1 = Checkbutton(root, text="C1")
+    c1.grid(row=3, sticky="w", pady=10)
+
+    root.mainloop()
+
+
 @app.route("/")
 def home():
+    test_fun()
+
     return "Hello, World!"
 
 
